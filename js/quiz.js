c6b8dfcce762276de36b351c7be70c93aa018212
nit-robotvolunteer <robotvolunteer.nit@gmail.com>
2025-12-20 19:17:52 +0900

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -255,7 +255,7 @@ function openCertModal() {
         ctx.fillStyle = "#1a2a44";
         ctx.textAlign = "right";
         // 「さん」の左側に配置（画像内の位置調整: xとy）
-        ctx.fillText(name, 500, 0); 
+        ctx.fillText(name, 100, 150); 
     };
 
     bgImg.onload = updateCanvas;
