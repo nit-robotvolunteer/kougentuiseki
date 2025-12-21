71ac774b20064dd979505c623229e773473ca0ad
nit-robotvolunteer <robotvolunteer.nit@gmail.com>
2025-12-21 14:26:00 +0900

    Update quiz.js

--- js/quiz.js
+++ js/quiz.js
@@ -268,10 +268,10 @@ function openCertModal() {
 
             const now = new Date();
             const dateString = `${now.getFullYear()}年${now.getMonth() + 1}月${now.getDate()}日`;
-            ctx.font = "16px 'Zen Maru Gothic', sans-serif"; // 日付用のフォントサイズ
+            ctx.font = "20px 'Zen Maru Gothic', sans-serif"; // 日付用のフォントサイズ
             ctx.fillStyle = "#1a2a44";
             ctx.textAlign = "center";
-            ctx.fillText(dateString, 300, 350); // タイトル上の位置に配置
+            ctx.fillText(dateString, 300, 320); // タイトル上の位置に配置
             
             const name = nameInput.value;
             ctx.font = "bold 24px 'Zen Maru Gothic', sans-serif";
